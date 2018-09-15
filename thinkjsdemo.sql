b1083d1c18a4fb8955445b327bf11950e5cc3553
tangpc <956126586@qq.com>
2018-09-15 15:56:28 +0800

    sql文件

---
+++ thinkjsdemo.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 蹦迪
+Source Server Version : 50722
+Source Host           : localhost:3306
+Source Database       : thinkjsdemo
+
+Target Server Type    : MYSQL
+Target Server Version : 50722
+File Encoding         : 65001
+
+Date: 2018-09-04 18:22:44
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
