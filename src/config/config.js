5b5fbf7df7dbb3af5291fbe720d98fa61eeed118
tangpc <956126586@qq.com>
2018-09-15 15:56:41 +0800

    可能需要的配置

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,20 @@
 // default config
 module.exports = {
-  workers: 1
+  workers: 1,
+
+  // 可以公开访问的Controller
+  publicController: [
+    // 格式为controller
+    'index',
+    'catalog'
+  ],
+
+  // 可以公开访问的Action
+  publicAction: [
+    // 格式为： controller+action
+    'comment/list',
+    'comment/count'
+  ],
+
+
 };
